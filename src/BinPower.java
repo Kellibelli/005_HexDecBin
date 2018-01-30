a9b2d3f4d94cf18913d72a891fd00ceb999d8f41
compsci <compsci@compsci>
2018-01-30 09:31:06 -0700

    Backup

--- src/BinPower.java
+++ src/BinPower.java
@@ -1,69 +1,34 @@
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
 public class BinPower {
-	//Fixed Scanner bug for float inputs
 	
 	Scanner input = new Scanner(System.in);
-	
-	
-	/*
-	 * String is cap'd, reason: It is a Class
-	 * int is not cap'd, reason: It is a "primitive" type, means it's not a Class
-	 * Integer -- full name, with cap is a Class!
-	 * double, lower case is also a "primitive" -- means NO Class for it
-	 * 
-	 */
-	
-	
-	public BinPower() {
-	
-	}
+	String name = "";
+	double base;
+	double exp;
+	int ans;
 
+	public BinPower() {
+		
+	}
+	
 	public void setBinary() {
 		
-		// 2**2 does not work, differs from Python
+		System.out.println("Hello Java Student! What is your name? ");
+		name = input.nextLine();
+		System.out.println("Thanks for using my program " + name + ".");
 		
-		double exp;
-		double base;
-		int ans = 0;
-		double answer;
-		
-
-		
-		
-		//ans = 2**2
-		//ans = 2^2;
-		//System.out.println("Answer is: " + ans);
-		//does not work - yet, no error msg
-		
-		//don't need 
-		//Math myMath = new Math();
-		//because it is "static" -- means just use it!
-		
-		/*
-		//System.out.println("Hello most amazing Java student! What is your name?");
-		JOptionPane.showMessageDialog(null, "Hello most amazing Java student! What is your name?");
-		String name = JOptionPane.showInputDialog("What is your name?");
-		JOptionPane.showMessageDialog(null, "Very nice to meet you " + name);
-		
-		System.out.println(name + " what base number do you want?");
+		System.out.println("What base do you want " + name + "?");
 		base = input.nextDouble();
-		System.out.println(name + " you have entered: " + base );
+		System.out.println(name + " you have entered " + base + " as your base." );
 		
-		System.out.println(name + " what binary exponent do you want?");
+		
+		System.out.println("Which exponent do you want " + name + "?");
 		exp = input.nextDouble();
-		System.out.println(name + " you have entered: " + exp );
+		System.out.println(name + " you have entered " + exp + " as your exponent." );
 		
-		// (int) "Type Casting" -- pow requires double, which makes no sense in binary
-		// so type cast to the correct answer
-		answer = Math.pow(base, exp );
-		System.out.println("Answer is: " + answer);
-		
-		*/
-		
-		
-	}
-	
+		ans = (int) Math.pow(base, exp);
+
+		System.out.println("Answer is: " + ans);	
+	}	
 }
--- src/HexDecBin.java
+++ src/HexDecBin.java
@@ -1,105 +1,35 @@
-import java.util.Scanner;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class HexDecBin {
 	
-	/*
-	 * 
-	 * FIX FAULTS! COMPLETE MISSING CODE!
-	 */
-	//Scanner for float is now working
-	Scanner kbd = new Scanner(System.in);
-	Boolean runAgain = true;
-	String goAgain = "";
-	String answer = "";
+	Scanner i = new Scanner(System.in);
 	String name = "";
 	String hex = "";
-	String dec = "";
-	String bin = "";
-	String msg = "";
 	int num;
-	int numberBits = 10;
-	int bite = 8; // spelled wrong to avoid a keyword conflict
-	int nibble = 4;
-	int byteAnswer = 0, byteRemainder = 0;
 	
+	public HexDecBin () {
+		System.out.println("Welcome to my Hexadecimal, Decimal, or Binary Conversion program.");
+		System.out.println("What is your name?");
+		name = i.nextLine();
+		System.out.println("Very nice to meet you, " + name + " let's begin: \n");
+	}
 	
-	
-	public HexDecBin() {
-		msg= "Welcome to my Hexadecimal, Decimal, or Binary conversion program!";
-		JOptionPane.showMessageDialog(null, msg);
-		String name = JOptionPane.showInputDialog("What is your name?");
-		JOptionPane.showMessageDialog(null, "Very nice to meet you " + name);
-		msg = "What do you want to start with, Hex, Dec, or Bin?";
-		goAgain = JOptionPane.showInputDialog(msg);
-		if (msg == ("Hex")) {
-			
-		}
-		//Left off here
-	} // end of HexDecBin
-	
-public void setHexNumber() {
-		
-		String hex = JOptionPane.showInputDialog(null, "Enter the hex number");
-		//hex = kbd.nextLine();
-		// mouse over parseInt to read pop-up guide
+	void setHexNumber () {
+		System.out.println("Enter the hex number: ");
+		hex = i.nextLine();
 		num = Integer.parseInt(hex, 16);
-		JOptionPane.showMessageDialog(null, "You have entered hex " + hex);
-		//System.out.println("You have entered hex " + hex );
-		JOptionPane.showMessageDialog(null, "Decimal value is: " + num + "(remember 0-" + num + " has " + (num+1) + " values)");
-		//System.out.println();
-	} //end of getHexNumber
-	
-	
-	
-	public void setDecNumber() {
-		String dec = JOptionPane.showInputDialog(null, "Enter the Dec number");
-		//System.out.println("\nEnter the Dec number");
-		//dec = kbd.nextLine();
-		
-		num = Integer.parseInt(dec);
-		//System.out.println("You have entered dec " + dec);
-		JOptionPane.showMessageDialog(null, "You have entered dec " + dec);
-		
-		
+		System.out.println("You have entered hex: " + hex);
+		System.out.println("Decimal value is: " + num + "(remember 0- " + num + " has " + (num+1) + " values)");
 	}
 	
-	public void setBinNumber() {
-		String bin = JOptionPane.showInputDialog(null, "Enter the Bin number");
-		//System.out.println("\nEnter the Bin number");
-		//bin = kbd.nextLine();
-	}
-	
-	void convert() {
-		
+	void convert () {
 		String binary = Integer.toBinaryString(num);
 		
-		JOptionPane.showMessageDialog(null, "Binary value is: " + binary);
-		//System.out.println("Binary value is: " + binary);
-		JOptionPane.showMessageDialog(null, "Number of bits is " + binary.length());
-		//System.out.println("Number of bits is " + binary.length());
-		JOptionPane.showMessageDialog(null, "Number of whole nibbles (4 bits) is "+ byteAnswer + " with " + numberBits + " bits left over");
-		//System.out.println("Number of whole nibbles (4 bits) is"+ byteAnswer + "with" + numberBits + "bits left over");
-		JOptionPane.showMessageDialog(null, "Number of whole bytes (8 bits) is " + byteRemainder + " with "+ numberBits +" bits remaining");
-		//System.out.println("Number of whole bytes (8 bits) is" + byteRemainder + "with"+ numberBits +"bits remaining");
-		byteAnswer = numberBits / bite;
-		byteRemainder = numberBits % bite;
-		
-		System.out.println("bits = " + numberBits + " bytes = " + byteAnswer + " remainder = " + byteRemainder);
-	}	
+		System.out.println("Binary value is: " + binary);
+		System.out.println("Number of bits is: " + binary.length());
+		System.out.println("Number of whole nibbles (4 bits) is x with y bits left over.");
+		System.out.println("Number of whole bytes (8 bits) is x with y bits remaining.");
+	}
 	
 }
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
